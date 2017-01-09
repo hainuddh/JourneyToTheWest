02245ef1b607cc2145bc06ef7979f7c9ff17264b
AnalectsOfConfucius <EssaysAndCriticism@gmail.com>
2017-01-09 14:07:57 +0800

    update doublerandom

---
+++ src/main/java/com/yyh/service/DoubleRandomService.java
@@ -0,0 +1,211 @@
+package com.yyh.service;
+
+import com.yyh.domain.Company;
+import com.yyh.domain.DoubleRandom;
+import com.yyh.domain.DoubleRandomResult;
+import com.yyh.domain.Manager;
+import com.yyh.repository.CompanyRepository;
+import com.yyh.repository.DoubleRandomRepository;
+import com.yyh.repository.DoubleRandomResultRepository;
+import com.yyh.repository.ManagerRepository;
+import com.yyh.repository.search.DoubleRandomSearchRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.elasticsearch.index.query.QueryBuilders.*;
+
+/**
+ * Service Implementation for managing DoubleRandom.
+ */
+@Service
+@Transactional
+public class DoubleRandomService {
+
+    private final Logger log = LoggerFactory.getLogger(DoubleRandomService.class);
+
+    @Inject
+    private DoubleRandomRepository doubleRandomRepository;
+
+    @Inject
+    private DoubleRandomSearchRepository doubleRandomSearchRepository;
+
+    @Inject
+    private ManagerRepository managerRepository;
+
+    @Inject
+    private CompanyRepository companyRepository;
+
+    @Inject
+    private DoubleRandomResultRepository doubleRandomResultRepository;
+
+    /**
+     * Save a doubleRandom with result.
+     *
+     * @return the persisted entity
+     */
+    public DoubleRandom saveDoubleRandomWithResult() {
+        DoubleRandom doubleRandom = new DoubleRandom();
+        doubleRandom.setDoubleRandomCompanyRatio("0.0001");
+        doubleRandom.setDoubleRandomDate("2016-1-9");
+        doubleRandom.setDoubleRandomManagerNumber("2");
+        doubleRandom.setDoubleRandomNotary("1");
+        doubleRandom.setDoubleRandomManagerRatio("1");
+        doubleRandom.setDoubleRandomName("dr");
+        log.debug("Request to save DoubleRandom : {}", doubleRandom);
+        List<Manager> managerList = managerRepository.findAll();
+        List<Company> companyList = companyRepository.findAll();
+        int[] people = randomPickPeople(managerList.size(), 1);
+        int[] business = randomPickBusiness(companyList.size(), 0.0001);
+        ArrayList<int[]> drResultList = bindPeopleWithBusiness(people, business);
+        for (int i = 0; i < drResultList.size(); i++) {
+            DoubleRandomResult doubleRandomResult = new DoubleRandomResult();
+            Set<Manager> managers = new HashSet<>();
+            Company company = companyList.get(drResultList.get(i)[0]);
+            managers.add(managerList.get(drResultList.get(i)[1]));
+            managers.add(managerList.get(drResultList.get(i)[2]));
+            doubleRandomResult.setPeople(managerList.get(drResultList.get(i)[1]).getManagerName() + "," + managerList.get(drResultList.get(i)[2]).getManagerName());
+            doubleRandomResult.setManagers(managers);
+            doubleRandomResult.setCompany(company);
+            doubleRandomResult.setCompanyRegisterId(company.getCompanyRegisterId());
+            doubleRandomResult.setCompanyName(company.getCompanyName());
+            doubleRandom.addDoubleRandomResult(doubleRandomResult);
+        }
+        doubleRandomResultRepository.save(doubleRandom.getDoubleRandomResults());
+        DoubleRandom result = doubleRandomRepository.save(doubleRandom);
+        return result;
+    }
+
+    /**
+     * Save a doubleRandom.
+     *
+     * @param doubleRandom the entity to save
+     * @return the persisted entity
+     */
+    public DoubleRandom save(DoubleRandom doubleRandom) {
+        log.debug("Request to save DoubleRandom : {}", doubleRandom);
+        DoubleRandom result = doubleRandomRepository.save(doubleRandom);
+        doubleRandomSearchRepository.save(result);
+        return result;
+    }
+
+    /**
+     *  Get all the doubleRandoms.
+     *
+     *  @param pageable the pagination information
+     *  @return the list of entities
+     */
+    @Transactional(readOnly = true)
+    public Page<DoubleRandom> findAll(Pageable pageable) {
+        log.debug("Request to get all DoubleRandoms");
+        Page<DoubleRandom> result = doubleRandomRepository.findAll(pageable);
+        return result;
+    }
+
+    /**
+     *  Get one doubleRandom by id.
+     *
+     *  @param id the id of the entity
+     *  @return the entity
+     */
+    @Transactional(readOnly = true)
+    public DoubleRandom findOne(Long id) {
+        log.debug("Request to get DoubleRandom : {}", id);
+        DoubleRandom doubleRandom = doubleRandomRepository.findOne(id);
+        return doubleRandom;
+    }
+
+    /**
+     *  Delete the  doubleRandom by id.
+     *
+     *  @param id the id of the entity
+     */
+    public void delete(Long id) {
+        log.debug("Request to delete DoubleRandom : {}", id);
+        doubleRandomRepository.delete(id);
+        doubleRandomSearchRepository.delete(id);
+    }
+
+    /**
+     * Search for the doubleRandom corresponding to the query.
+     *
+     *  @param query the query of the search
+     *  @return the list of entities
+     */
+    @Transactional(readOnly = true)
+    public Page<DoubleRandom> search(String query, Pageable pageable) {
+        log.debug("Request to search for a page of DoubleRandoms for query {}", query);
+        Page<DoubleRandom> result = doubleRandomSearchRepository.search(queryStringQuery(query), pageable);
+        return result;
+    }
+
+    /*
+    @totalNum   人员或者公司总共的数量
+    @pickRatio  挑选的概率
+    该函数生成一个范围为0~totalNum-1的数量为ceil(totalNum * pickRatio)的数组。
+     */
+    private int[] generateRandomArray(int totalNum, double pickRatio) {
+        int pickNum = (int) Math.ceil(totalNum * pickRatio);
+        int[] peopleArray = new int[totalNum];
+        int[] pickArray = new int[pickNum];
+        int shuffleTime = 5;
+        for (int i = 0; i < totalNum; i++) {
+            peopleArray[i] = i;
+        }
+        Random r = new Random();
+        for (int shuffle = 0; shuffle < shuffleTime; shuffle++) {
+            for (int i = 0; i < totalNum; i++) {
+                // doubleRandom switch
+                int j = r.nextInt(totalNum);
+                int temp = peopleArray[i];
+                peopleArray[i] = peopleArray[j];
+                peopleArray[j] = temp;
+            }
+        }
+        for (int i = 0; i < pickNum; i++) {
+            pickArray[i] = peopleArray[i];
+        }
+        return pickArray;
+    }
+
+    private int[] randomPickPeople(int totalNumOfPeople, double pickRatio) {
+        return generateRandomArray(totalNumOfPeople, pickRatio);
+    }
+
+    private int[] randomPickBusiness(int totalNumOfBusiness, double pickRatio) {
+        return generateRandomArray(totalNumOfBusiness, pickRatio);
+    }
+
+    private ArrayList<int[]> bindPeopleWithBusiness(int[] peopleArray, int[] businessArray) {
+        ArrayList<int[]> bindResult = new ArrayList<int[]>();
+        int peopleCounter = 0;
+        int[] randomArray = generateRandomArray(peopleArray.length, 1);
+        for (int i = 0; i < businessArray.length; i++) {
+            int[] pair = new int[3];
+            int n = peopleArray.length;
+            pair[0] = businessArray[i];
+            pair[1] = peopleArray[randomArray[peopleCounter]];
+            peopleCounter++;
+            if (peopleCounter >= n) {
+                peopleCounter -= n;
+                randomArray = generateRandomArray(peopleArray.length, 1);
+            }
+            pair[2] = peopleArray[randomArray[peopleCounter]];
+            peopleCounter++;
+            if (peopleCounter >= n) {
+                peopleCounter -= n;
+                randomArray = generateRandomArray(peopleArray.length, 1);
+            }
+            bindResult.add(pair.clone());
+        }
+        return bindResult;
+    }
+}
--- src/main/java/com/yyh/web/rest/DoubleRandomResource.java
+++ src/main/java/com/yyh/web/rest/DoubleRandomResource.java
@@ -1,15 +1,8 @@
 package com.yyh.web.rest;
 
 import com.codahale.metrics.annotation.Timed;
-import com.yyh.domain.Company;
 import com.yyh.domain.DoubleRandom;
-
-import com.yyh.domain.DoubleRandomResult;
-import com.yyh.domain.Manager;
-import com.yyh.repository.CompanyRepository;
-import com.yyh.repository.DoubleRandomRepository;
-import com.yyh.repository.ManagerRepository;
-import com.yyh.repository.search.DoubleRandomSearchRepository;
+import com.yyh.service.DoubleRandomService;
 import com.yyh.web.rest.util.HeaderUtil;
 import com.yyh.web.rest.util.PaginationUtil;
 
@@ -27,7 +20,8 @@ import javax.inject.Inject;
 import javax.validation.Valid;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -43,49 +37,18 @@ public class DoubleRandomResource {
     private final Logger log = LoggerFactory.getLogger(DoubleRandomResource.class);
 
     @Inject
-    private DoubleRandomRepository doubleRandomRepository;
-
-    @Inject
-    private DoubleRandomSearchRepository doubleRandomSearchRepository;
-
-    @Inject
-    private ManagerRepository managerRepository;
-
-    @Inject
-    private CompanyRepository companyRepository;
+    private DoubleRandomService doubleRandomService;
 
     /**
-     * POST  /double-randoms/start : Create a new doubleRandom with result.
+     * POST  /double-randoms/start : Create a new doubleRandom.
      *
-     * @param doubleRandom the doubleRandom to create
      * @return the ResponseEntity with status 201 (Created) and with body the new doubleRandom, or with status 400 (Bad Request) if the doubleRandom has already an ID
      * @throws URISyntaxException if the Location URI syntax is incorrect
      */
     @PostMapping("/double-randoms/start")
     @Timed
-    public ResponseEntity<DoubleRandom> start(@Valid @RequestBody DoubleRandom doubleRandom) throws URISyntaxException {
-        log.debug("REST request to save DoubleRandom : {}", doubleRandom);
-        if (doubleRandom.getId() != null) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("doubleRandom", "idexists", "A new doubleRandom cannot already have an ID")).body(null);
-        }
-        List<Manager> managerList = managerRepository.findAll();
-        List<Company> companyList = companyRepository.findAll();
-        int[] people = randomPickPeople(managerList.size(), 1);
-        int[] business = randomPickBusiness(companyList.size(), 0.01);
-        ArrayList<int[]> drResultList = bindPeopleWithBusiness(people, business);
-        List<DoubleRandomResult> doubleRandomResultList = new ArrayList<>();
-        for (int i = 0; i < drResultList.size(); i++) {
-            DoubleRandomResult doubleRandomResult = new DoubleRandomResult();
-            Set<Manager> managers = null;
-            Company company = companyList.get(drResultList.get(i)[0]);
-            managers.add(managerList.get(drResultList.get(i)[1]));
-            managers.add(managerList.get(drResultList.get(i)[2]));
-            doubleRandomResult.setManagers(managers);
-            doubleRandomResult.setCompany(company);
-            doubleRandomResultList.add(doubleRandomResult);
-        }
-        DoubleRandom result = doubleRandomRepository.save(doubleRandom);
-        doubleRandomSearchRepository.save(result);
+    public ResponseEntity<DoubleRandom> startDoubleRandom() throws URISyntaxException {
+        DoubleRandom result = doubleRandomService.saveDoubleRandomWithResult();
         return ResponseEntity.created(new URI("/api/double-randoms/" + result.getId()))
             .headers(HeaderUtil.createEntityCreationAlert("doubleRandom", result.getId().toString()))
             .body(result);
@@ -105,8 +68,7 @@ public class DoubleRandomResource {
         if (doubleRandom.getId() != null) {
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("doubleRandom", "idexists", "A new doubleRandom cannot already have an ID")).body(null);
         }
-        DoubleRandom result = doubleRandomRepository.save(doubleRandom);
-        doubleRandomSearchRepository.save(result);
+        DoubleRandom result = doubleRandomService.save(doubleRandom);
         return ResponseEntity.created(new URI("/api/double-randoms/" + result.getId()))
             .headers(HeaderUtil.createEntityCreationAlert("doubleRandom", result.getId().toString()))
             .body(result);
@@ -128,8 +90,7 @@ public class DoubleRandomResource {
         if (doubleRandom.getId() == null) {
             return createDoubleRandom(doubleRandom);
         }
-        DoubleRandom result = doubleRandomRepository.save(doubleRandom);
-        doubleRandomSearchRepository.save(result);
+        DoubleRandom result = doubleRandomService.save(doubleRandom);
         return ResponseEntity.ok()
             .headers(HeaderUtil.createEntityUpdateAlert("doubleRandom", doubleRandom.getId().toString()))
             .body(result);
@@ -147,7 +108,7 @@ public class DoubleRandomResource {
     public ResponseEntity<List<DoubleRandom>> getAllDoubleRandoms(@ApiParam Pageable pageable)
         throws URISyntaxException {
         log.debug("REST request to get a page of DoubleRandoms");
-        Page<DoubleRandom> page = doubleRandomRepository.findAll(pageable);
+        Page<DoubleRandom> page = doubleRandomService.findAll(pageable);
         HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/double-randoms");
         return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
     }
@@ -162,7 +123,7 @@ public class DoubleRandomResource {
     @Timed
     public ResponseEntity<DoubleRandom> getDoubleRandom(@PathVariable Long id) {
         log.debug("REST request to get DoubleRandom : {}", id);
-        DoubleRandom doubleRandom = doubleRandomRepository.findOne(id);
+        DoubleRandom doubleRandom = doubleRandomService.findOne(id);
         return Optional.ofNullable(doubleRandom)
             .map(result -> new ResponseEntity<>(
                 result,
@@ -180,8 +141,7 @@ public class DoubleRandomResource {
     @Timed
     public ResponseEntity<Void> deleteDoubleRandom(@PathVariable Long id) {
         log.debug("REST request to delete DoubleRandom : {}", id);
-        doubleRandomRepository.delete(id);
-        doubleRandomSearchRepository.delete(id);
+        doubleRandomService.delete(id);
         return ResponseEntity.ok().headers(HeaderUtil.createEntityDeletionAlert("doubleRandom", id.toString())).build();
     }
 
@@ -189,7 +149,7 @@ public class DoubleRandomResource {
      * SEARCH  /_search/double-randoms?query=:query : search for the doubleRandom corresponding
      * to the query.
      *
-     * @param query    the query of the doubleRandom search
+     * @param query the query of the doubleRandom search
      * @param pageable the pagination information
      * @return the result of the search
      * @throws URISyntaxException if there is an error to generate the pagination HTTP headers
@@ -199,71 +159,10 @@ public class DoubleRandomResource {
     public ResponseEntity<List<DoubleRandom>> searchDoubleRandoms(@RequestParam String query, @ApiParam Pageable pageable)
         throws URISyntaxException {
         log.debug("REST request to search for a page of DoubleRandoms for query {}", query);
-        Page<DoubleRandom> page = doubleRandomSearchRepository.search(queryStringQuery(query), pageable);
+        Page<DoubleRandom> page = doubleRandomService.search(query, pageable);
         HttpHeaders headers = PaginationUtil.generateSearchPaginationHttpHeaders(query, page, "/api/_search/double-randoms");
         return new ResponseEntity<>(page.getContent(), headers, HttpStatus.OK);
     }
 
-    /*
-    @totalNum   人员或者公司总共的数量
-    @pickRatio  挑选的概率
-    该函数生成一个范围为0~totalNum-1的数量为ceil(totalNum * pickRatio)的数组。
-     */
-    private int[] generateRandomArray(int totalNum, double pickRatio) {
-        int pickNum = (int) Math.ceil(totalNum * pickRatio);
-        int[] peopleArray = new int[totalNum];
-        int[] pickArray = new int[pickNum];
-        int shuffleTime = 5;
-        for (int i = 0; i < totalNum; i++) {
-            peopleArray[i] = i;
-        }
-        Random r = new Random();
-        for (int shuffle = 0; shuffle < shuffleTime; shuffle++) {
-            for (int i = 0; i < totalNum; i++) {
-                // doubleRandom switch
-                int j = r.nextInt(totalNum);
-                int temp = peopleArray[i];
-                peopleArray[i] = peopleArray[j];
-                peopleArray[j] = temp;
-            }
-        }
-        for (int i = 0; i < pickNum; i++) {
-            pickArray[i] = peopleArray[i];
-        }
-        return pickArray;
-    }
-
-    private int[] randomPickPeople(int totalNumOfPeople, double pickRatio) {
-        return generateRandomArray(totalNumOfPeople, pickRatio);
-    }
-
-    private int[] randomPickBusiness(int totalNumOfBusiness, double pickRatio) {
-        return generateRandomArray(totalNumOfBusiness, pickRatio);
-    }
-
-    private ArrayList<int[]> bindPeopleWithBusiness(int[] peopleArray, int[] businessArray) {
-        ArrayList<int[]> bindResult = new ArrayList<int[]>();
-        int peopleCounter = 0;
-        int[] randomArray = generateRandomArray(peopleArray.length, 1);
-        for (int i = 0; i < businessArray.length; i++) {
-            int[] pair = new int[3];
-            int n = peopleArray.length;
-            pair[0] = businessArray[i];
-            pair[1] = peopleArray[randomArray[peopleCounter]];
-            peopleCounter++;
-            if (peopleCounter >= n) {
-                peopleCounter -= n;
-                randomArray = generateRandomArray(peopleArray.length, 1);
-            }
-            pair[2] = peopleArray[randomArray[peopleCounter]];
-            peopleCounter++;
-            if (peopleCounter >= n) {
-                peopleCounter -= n;
-                randomArray = generateRandomArray(peopleArray.length, 1);
-            }
-            bindResult.add(pair.clone());
-        }
-        return bindResult;
-    }
 
 }
